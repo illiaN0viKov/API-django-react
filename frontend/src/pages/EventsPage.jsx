7b4f4c4ef352254b6ae6decede2693581208a446
Illia Novikov <166924986+illiaN0viKov@users.noreply.github.com>
2024-12-28 08:38:33 +0000

    commitski

--- frontend/src/pages/EventsPage.jsx
+++ frontend/src/pages/EventsPage.jsx
@@ -1,44 +1,45 @@
-import React, {useState, useEffect} from 'react'
-import EventItem from '../components/EventItem'
-import Pagination from '../components/Pagination'
-
-
+import React, { useState, useEffect } from 'react';
+import EventItem from '../components/EventItem';
+import Pagination from '../components/Pagination';
 
 const EventsPage = () => {
+  let [events, setEvents] = useState([]);
+  let [currentPage, setCurrentPage] = useState(1);
+  let [eventsPerPage] = useState(8);
 
-  let [events, setEvents] = useState([])
-  let [currentPage, setCurrentPage] = useState(1)
-  let [eventsPerPage, setEventsPerPage] = useState(8)
+  useEffect(() => {
+    getEvents();
+  }, []);
 
-  useEffect(() => { 
-    getEvents()
-  }, [])
+  const getEvents = async () => {
+    let response = await fetch("/api/events-list/");
+    let data = await response.json();
+    setEvents(data);
+  };
 
+  // Get Current Events (Pagination)
+  let indexOfLastEvent = currentPage * eventsPerPage;
+  let indexOfFirstEvent = indexOfLastEvent - eventsPerPage;
+  let currentEvents = events.slice(indexOfFirstEvent, indexOfLastEvent);
 
-  let getEvents = async () => {
-
-    let response = await fetch("/api/events-list/")
-    let data = await response.json()
-    setEvents(data)
-
-  }
-
-  //Get Current Events (Pagination)
-  let indexOfLastEvent = currentPage * eventsPerPage
-  let indexOfFirstEvent = indexOfLastEvent - eventsPerPage
-  let currentEvents = events.slice(indexOfFirstEvent, indexOfLastEvent)
-
-  let paginate = (pageNumber) => setCurrentPage(pageNumber)
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
     <div className="flex flex-col min-h-screen">
-      <div className="grid lg:grid-cols-4 gap-8 container mx-auto  ">
-        {currentEvents.map((event) => (
-          <EventItem key={event.id} event={event} />
-        ))}
+      
+      {/* Content Wrapper */}
+      <div>
+        <div className="grid lg:grid-cols-4 gap-8 container mx-auto">
+          {currentEvents.map((event) => (
+            <EventItem key={event.id} event={event} />
+          ))}
+        </div>
+
+        
       </div>
 
-      <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2">
+      {/* Pagination */}
+      <div className="py-4 container mx-auto">
         <Pagination
           eventsPerPage={eventsPerPage}
           totalEvents={events.length}
@@ -48,6 +49,6 @@ const EventsPage = () => {
       </div>
     </div>
   );
-}
+};
 
-export default EventsPage
+export default EventsPage;
